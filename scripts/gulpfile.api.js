d44cf135d761d6379b0017699f172884b845c4f4
Greg Van Liew <gregvanl@microsoft.com>
2016-03-23 21:10:38 -0700

    Remove extra space in vscode.d.ts commit id.

--- scripts/gulpfile.api.js
+++ scripts/gulpfile.api.js
@@ -17,7 +17,7 @@ var VSCODE_PATH = 'out/vscode.d.ts';
 var TEMPLATE = 'docs/extensionAPI/vscode-api.template';
 var DIST = 'docs/extensionAPI/vscode-api.md';
 
-var VSCODE_VERSION = process.env['vscode-lastest'] ? process.env['vscode-lastest'] : 'aceb73693678fc037384314b820d3b6bfd7f11ff ' /* 0.10.12 */; 
+var VSCODE_VERSION = process.env['vscode-lastest'] ? process.env['vscode-lastest'] : 'aceb73693678fc037384314b820d3b6bfd7f11ff' /* 0.10.12 */; 
 
 function getVSCodeDefFileURL() {
     if (VSCODE_VERSION === "latest") {
