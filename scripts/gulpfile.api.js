783aef669e71d9f6d21174970c76413c6a814f7d
Greg Van Liew <gregvanl@microsoft.com>
2016-10-01 21:08:30 -0700

    Update vscode.d.ts commit id for 1.6

--- scripts/gulpfile.api.js
+++ scripts/gulpfile.api.js
@@ -17,7 +17,7 @@ var VSCODE_PATH = 'out/vscode.d.ts';
 var TEMPLATE = 'docs/extensionAPI/vscode-api.template';
 var DIST = 'docs/extensionAPI/vscode-api.md';
 
-var VSCODE_VERSION = process.env['vscode-lastest'] ? process.env['vscode-lastest'] : '4fc690be310dd02e0ab6529c0b9bf348a8b26a19' /* 1.5.0 */; 
+var VSCODE_VERSION = process.env['vscode-lastest'] ? process.env['vscode-lastest'] : '35540a82a4e865f11fb631906007a4c4f9f906f9' /* 1.6.0 */; 
 
 function getVSCodeDefFileURL() {
     if (VSCODE_VERSION === "latest") {
