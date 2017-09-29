10d03329cca99d5c6642171cc8ecc7f313f20122
Greg Van Liew <gregvanl@microsoft.com>
2017-09-29 14:38:55 -0700

    Edit pass on 1.17 notes Including reviewing NOT MERGED - PLS REVIEW with Christof

--- scripts/gulpfile.api.js
+++ scripts/gulpfile.api.js
@@ -17,7 +17,7 @@ var VSCODE_PATH = 'out/vscode.d.ts';
 var TEMPLATE = 'docs/extensionAPI/vscode-api.template';
 var DIST = 'docs/extensionAPI/vscode-api.md';
 
-var VSCODE_VERSION = process.env['vscode-lastest'] ? process.env['vscode-lastest'] : '4ed4719a5f29ba7736e2763a330e242c6db4b858' /* 1.17.0 */;
+var VSCODE_VERSION = process.env['vscode-lastest'] ? process.env['vscode-lastest'] : '7ac1f1e05baad03d4dd8f8515f1e598180b2c97e' /* 1.17.0 */;
 
 function getVSCodeDefFileURL() {
     if (VSCODE_VERSION === "latest") {
