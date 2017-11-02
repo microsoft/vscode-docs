e5dd109fe9c83120ba0d7802b2c737ff35f2beb5
Greg Van Liew <gregvanl@microsoft.com>
2017-11-01 22:20:43 -0700

    Initial 1.18 vscode.d.ts commit id

--- scripts/gulpfile.api.js
+++ scripts/gulpfile.api.js
@@ -17,7 +17,7 @@ var VSCODE_PATH = 'out/vscode.d.ts';
 var TEMPLATE = 'docs/extensionAPI/vscode-api.template';
 var DIST = 'docs/extensionAPI/vscode-api.md';
 
-var VSCODE_VERSION = process.env['vscode-lastest'] ? process.env['vscode-lastest'] : 'be377c0faf7574a59f84940f593a6849f12e4de7' /* 1.17.0 */;
+var VSCODE_VERSION = process.env['vscode-lastest'] ? process.env['vscode-lastest'] : '2ab10d50163f008ada9a4aa215de8c884857ce99' /* 1.18.0 */;
 
 function getVSCodeDefFileURL() {
     if (VSCODE_VERSION === "latest") {
