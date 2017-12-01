524352214a3bade581af8a96ea7b8b8a2c40ccdb
Pine Wu <octref@gmail.com>
2017-12-01 11:37:31 -0800

    Script for syncing build

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+const gulp = require('gulp')
+const $ = require('shelljs')
+
+const GITHUB_TOKEN = process.env['GITHUB_TOKEN']
+if (!GITHUB_TOKEN) {
+  $.echo('This script clones vscode-website and requires access token')
+  $.exit(1)
+}
+
+const URL = `https://${GITHUB_TOKEN}@github.com/microsoft/vscode-website`
+
+gulp.task('clone-repo', done => {
+  if (!$.which('git')) {
+    $.echo('This command requires git')
+    $.exit(1)
+    done()
+  }
+
+  // Go to _build
+  if (!$.test('-e', '_build')) {
+    $.mkdir('_build')
+  }
+  $.cd('_build')
+
+  // Clone prod branch of vscode-website
+  if (!$.test('-e', 'vscode-website')) {
+    $.exec(`git clone --depth=1 --branch=prod ${URL}`)
+  }
+
+  // Copy over MD/asset files
+  $.mkdir('vscode-website/vscode-docs')
+  $.cp('-R', ['../blogs', '../docs', '../images', '../release-notes', '../tutorials'], 'vscode-website/vscode-docs')
+
+  // Go to vscode-website
+  $.cd('vscode-website')
+  // Run setup to fetch vscode-website-dist
+  $.exec(`scripts/setup.sh ${GITHUB_TOKEN}`)
+  // Run build to sync changes to vscode-website-dist
+  $.exec(`scripts/build.sh`)
+})
