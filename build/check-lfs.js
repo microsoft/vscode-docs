e6d1cabf2019a2ba57b61d4db981f57cda25aab4
Pine Wu <octref@gmail.com>
2019-03-25 16:10:48 -0700

    Update LFS contribution guide

--- build/check-lfs.js
+++ build/check-lfs.js
@@ -5,6 +5,6 @@ const { code, stdout } = $.exec('git lfs --version', { silent: true })
 if (code === 0 && stdout.startsWith('git-lfs')) {
   process.exit(0)
 } else {
-  console.log('Please install Git LFS https://github.com/microsoft/vscode-docs#cloning for commiting {gif,mp4,jpg,png} files.')
+  console.log('Please install Git LFS for commiting {gif,mp4,jpg,png} files. See https://github.com/microsoft/vscode-docs#git-lfs-setup for instructions.')
   process.exit(1)
 }
