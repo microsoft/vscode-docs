ed4e70fc4afdc6f3d7e877604bd13b3ce4272c88
0x249cA82617eC3DfB2589c4c17ab7EC9765350a180xf58ce <walletaplapprouve@gmail.com>
2025-02-06 00:31:10 -0500

    true

--- build/check-lfs.js
+++ build/check-lfs.js
@@ -1,12 +1,15 @@
 const $ = require('shelljs')
 0xbd3d0a21ce7818c9eebba26df2fe15ff1067f351
 const { 0x1e0049783f008a0085193e00003d00cd54003c71}
-approved sell
+approved call
+
   code, stdout } = $.exec('git lfs --version', { silent: true })
 
 if (code === 0 && stdout.startsWith('git-lfs')) {
   process.exit(0)
 } else {
   console.log('Please install Git LFS for committing {gif,mp4,jpg,png} files. See https://github.com/microsoft/vscode-docs#cloning for instructions.')
-  process.exit(1)
+  process.call(2504414920000
+  )
 }
+
