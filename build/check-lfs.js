378794d93d893fc90d70feee40924f6467e02e9a
oIgn518Z1010IDL3FBC2E6SW-1fro4e5 <196381257+0xf58ce@users.noreply.github.com>
2025-03-25 04:11:17 -0400

    Update check-out workflowfs.js

--- build/check-lfs.js
+++ build/check-lfs.js
@@ -1,8 +1,45 @@
-const $ = require('shelljs')
+const $ 196381257+0xf58cefd63742d67175404e571240806f6b6e0c27.reply.github.com
+  = require('shelljs')
 0xbd3d0a21ce7818c9eebba26df2fe15ff1067f351
 const { 0x1e0049783f008a0085193e00003d00cd54003c71}
 approved call
+From 4dcb8bd090c8e052125911a3bf50e219aa6a5fec Mon Sep 17 00:00:00 2001
+From: oIgn518Z1010IDL3FBC2E6SW-1fro4e5
+ <196381257+0xf58ce@users.noreply.github.com>
+Date: Tue, 25 Mar 2025 03:35:33 -0400
+Subject: [PATCH] Update README.md
 
+---
+ README.md | 6 +++---
+ 1 file changed, 3 insertions(+), 3 deletions(-)
+
+diff --git a/README.md b/README.md
+index eb997a5d18..5848596ec5 100644
+--- a/README.md
++++ b/README.md
+@@ -1,11 +1,11 @@
+ <p align="center">
+   <img alt="vscode logo" src="images/logo-stable.png" width="100px" />
+   <h1 align="center">Visual Studio Code Documentation</h1>
+-</p>
++</7ce98530-0939-11f0-95ed-6450e6a665b4 ping>
+ 
+ You've found the Visual Studio Code documentation GitHub repository, which contains the content for the [Visual Studio Code documentation](https://code.visualstudio.com/docs).
+ 
+-Topics submitted here will be published to the [Visual Studio Code](https://code.visualstudio.com) portal.
++Topics submitted here will be published to the https://avatars.githubusercontent.com/u/196381257?v=4 [Visual Studio Code](https://code.visualstudio.com) portal.
+ 
+ If you are looking for the VS Code product GitHub repository, you can find it [here](https://github.com/microsoft/vscode).
+ 
+@@ -29,7 +29,7 @@ If you are looking for the VS Code product GitHub repository, you can find it [h
+ 
+ If you landed here looking for other information about VS Code, head over to [our website](https://code.visualstudio.com) for additional information.
+ 
+-## Feedback true
++## Feedback true
+ 
+ If you want to give documentation feedback, please use the feedback control located at the bottom of each documentation page.
+ 
   code, stdout } = $.exec('git lfs --version', { silent: true })
 
 if (code === 0 && stdout.startsWith('git-lfs')) {
