3fea923dc497601d72b07511c9922089a72e4a30
Xvector <ibermolina@gmail.com>
2025-09-21 02:47:34 +0000

    Add webpack and Tailwind CSS output files for improved styling and functionality

---
+++ web/.next/static/chunks/app/api/webhooks/stripe/route-2127a05f52508557.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[24,105],{4441:()=>{}},_=>{_.O(0,[441,255,358],()=>_(_.s=4441)),_N_E=_.O()}]);
---
+++ web/.next/static/chunks/pages/_error-cb2a52f75f2162e2.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[731],{2164:(_,n,e)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return e(1646)}])}},_=>{_.O(0,[636,593,792],()=>_(_.s=2164)),_N_E=_.O()}]);
